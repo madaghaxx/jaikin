5aa4468e9fb8175f32db27908ae133f197c774c3
xySaad <saadrm.dev@gmail.com>
2025-11-17 13:57:29 +0100

    no idea

--- src/jaikin/App.java
+++ src/jaikin/App.java
@@ -21,7 +21,6 @@ public class App extends Frame implements Displayable {
     private List<Circle> points = new ArrayList<>();
     private List<Point> pixels = new ArrayList<>();
     private boolean isAnimating = false;
-    private int step = 0;
 
     public App(String title) {
         super(title);
@@ -50,12 +49,7 @@ public class App extends Frame implements Displayable {
         new Thread(() -> {
             while (true) {
                 List<Circle> chaikinPoints = new ArrayList<>(points);
-                for (int i = 0; i < chaikinPoints.size() - 1; i++) {
-                    Circle p1 = chaikinPoints.get(i);
-                    Circle p2 = chaikinPoints.get(i + 1);
-                    new Line(p1.getCenter(), p2.getCenter(), LINE_THICKNESS).draw(this);
-                }
-
+                drawShape(chaikinPoints);
                 repaint();
                 try {
                     Thread.sleep(1000);
@@ -69,30 +63,15 @@ public class App extends Frame implements Displayable {
                     chaikinPoints.clear();
                     chaikinPoints.add(copy.get(0));
                     for (int i = 0; i < copy.size() - 1; i++) {
-                        Circle c1 = copy.get(i);
-                        Circle c2 = copy.get(i + 1);
-                        Point p1 = c1.getCenter();
-                        Point p2 = c2.getCenter();
-
-                        double dx = p2.x - p1.x;
-                        double dy = p2.y - p1.y;
-
-                        double xQuarter = p1.x + dx / 4.0; // 1/4 from p1
-                        double yQuarter = p1.y + dy / 4.0;
-                        double xThreeQ = p1.x + 3.0 * dx / 4.0; // 3/4 from p1
-                        double yThreeQ = p1.y + 3.0 * dy / 4.0;
-
-                        chaikinPoints.add(circledPoint((int) Math.round(xQuarter), (int) Math.round(yQuarter)));
-                        chaikinPoints.add(circledPoint((int) Math.round(xThreeQ), (int) Math.round(yThreeQ)));
+                        Point p1 = copy.get(i).getCenter();
+                        Point p2 = copy.get(i + 1).getCenter();
+                        Circle[] newPs = chaikinStep(p1, p2);
+                        chaikinPoints.add(newPs[0]);
+                        chaikinPoints.add(newPs[1]);
                     }
                     chaikinPoints.add(copy.get(copy.size() - 1));
 
-                    // draw new shape
-                    for (int i = 0; i < chaikinPoints.size() - 1; i++) {
-                        Circle p1 = chaikinPoints.get(i);
-                        Circle p2 = chaikinPoints.get(i + 1);
-                        new Line(p1.getCenter(), p2.getCenter(), LINE_THICKNESS).draw(this);
-                    }
+                    drawShape(chaikinPoints);
                     repaint();
 
                     try {
@@ -103,6 +82,28 @@ public class App extends Frame implements Displayable {
                 }
             }
         }).start();
+    }
+
+    public Circle[] chaikinStep(Point p1, Point p2) {
+        double dx = p2.x - p1.x;
+        double dy = p2.y - p1.y;
+
+        double xQuarter = p1.x + dx / 4.0;
+        double yQuarter = p1.y + dy / 4.0;
+        double xThreeQ = p1.x + 3.0 * dx / 4.0;
+        double yThreeQ = p1.y + 3.0 * dy / 4.0;
+
+        Circle newp1 = circledPoint((int) Math.round(xQuarter), (int) Math.round(yQuarter));
+        Circle newp2 = circledPoint((int) Math.round(xThreeQ), (int) Math.round(yThreeQ));
+        return new Circle[] { newp1, newp2 };
+    }
+
+    public void drawShape(List<Circle> points) {
+        for (int i = 0; i < points.size() - 1; i++) {
+            Circle p1 = points.get(i);
+            Circle p2 = points.get(i + 1);
+            new Line(p1.getCenter(), p2.getCenter(), LINE_THICKNESS).draw(this);
+        }
     }
 
     @Override
